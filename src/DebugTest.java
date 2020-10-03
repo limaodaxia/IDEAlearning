e101b926c1e63561ec8b151ea2e6ac5d79b2edd8
limaodaxia <1161855028@qq.com>
2020-10-03 23:47:00 +0800

    IDEAlearning has been finished

---
+++ src/DebugTest.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+/**
+ * @author lxl
+ * @create 2020/10/3 - 21:28
+ */
+public class DebugTest {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("i = " + i);
+        }
+       HashMap<String,String> map=new HashMap<>();
+       map.put("name","Tom");
+       map.put("age","12");
+       map.put("school","Tsinghua");
+       map.put("major","computer");
+
+        String age = map.get("age");
+        System.out.println("age = " + age);
+
+        map.remove("major");
+        System.out.println(map);
+    }
+}
